ab81754da43fa1a03e6dd0f486de9bfbb1b2495d
ravinromuald-png <ravinromuald@gmail.com>
2026-03-18 19:15:28 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from .config import Config
 from .routes import bp
+from .db import init_db, close_db
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
     app.register_blueprint(bp)
+
+    init_db(app)
+    app.teardown_appcontext(close_db)
+
     return app
